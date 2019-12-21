41b46a481af4411fd9156593feb0775ad844bcaa
thanq <thanq@DESKTOP-42KOIL4>
2019-12-21 15:54:54 +0000

    Version 2 KATA5

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -6,6 +6,7 @@ public class Kata5P1 {
 
     public static void main(String[] args) {
         connect();
+        createNewTable();
     }
     private static Connection connect() {
         Connection conn = null;
@@ -27,7 +28,7 @@ public class Kata5P1 {
         }
         return conn;
     }
-    public static void selectAll(Connection conn){
+    private static void selectAll(Connection conn){
         String sql = "SELECT * FROM PEOPLE";
         try {
         Statement stmt = conn.createStatement();
@@ -40,6 +41,19 @@ public class Kata5P1 {
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        }
+    }
+    public static void createNewTable() {
+        String url = "jdbc:sqlite:C:\\Users\\thanq\\Desktop\\Kata5P1\\mail.db";
+        String sql = "CREATE TABLE IF NOT EXISTS direcc_email (\n"
+            + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+            + " direccion text NOT NULL);";
+        try (Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e) {
+        System.out.println(e.getMessage());
         }
     }
 }
